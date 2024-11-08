60d5d64bf5dd6f7818abb944e103f4de2993d41d
w4ng3 <w2gdong@gmail.com>
2024-11-08 08:33:58 +0800

    upt:app.vue2

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
     <img alt="Vue logo" class="logo mxc" src="@/assets/logo.svg" width="125" height="125" />
     <div class="wrapper">
-      <HelloWorld msg="You did it!" class="bac" />
+      <HelloWorld msg="You did it!" class="bac 123214" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
